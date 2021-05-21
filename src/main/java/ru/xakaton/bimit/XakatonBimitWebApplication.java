c25e56a2c6d36c76dc314db5ba2c94b191674ed7
Master Maks <max@bimit.ru>
2021-05-21 19:54:32 +0400

    start

---
+++ src/main/java/ru/xakaton/bimit/XakatonBimitWebApplication.java
@@ -0,0 +1,13 @@
+package ru.xakaton.bimit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XakatonBimitWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XakatonBimitWebApplication.class, args);
+	}
+
+}
