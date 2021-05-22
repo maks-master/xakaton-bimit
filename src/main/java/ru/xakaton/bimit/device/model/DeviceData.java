ec94e85189bd80679a72ba42da5add9b0bababea
Master Maks <master@mifors.com>
2021-05-22 16:07:58 +0400

    d

--- src/main/java/ru/xakaton/bimit/device/model/DeviceData.java
+++ src/main/java/ru/xakaton/bimit/device/model/DeviceData.java
@@ -8,6 +8,8 @@ import javax.persistence.Id;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import ru.xakaton.bimit.device.enums.DeviceState;
+
 @Entity
 public class DeviceData {
 	@Id
@@ -21,6 +23,26 @@ public class DeviceData {
 	
 	@DateTimeFormat()
 	private java.sql.Timestamp time;
+
+	private DeviceState deviceState;
+	
+	public DeviceData() {
+		super();
+	}
+	
+	public DeviceData(UUID deviceUuid) {
+		super();
+		this.deviceUuid = deviceUuid;
+	}
+	
+	
+	public DeviceState getDeviceState() {
+		return deviceState;
+	}
+
+	public void setDeviceState(DeviceState deviceState) {
+		this.deviceState = deviceState;
+	}
 
 	public UUID getUuid() {
 		return uuid;
