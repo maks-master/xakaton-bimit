cc537ee842ae389dcb3b432a5793931df9cddf18
Master Maks <master@mifors.com>
2021-05-23 00:21:34 +0400

    sss

--- src/main/java/ru/xakaton/bimit/service/ModelService.java
+++ src/main/java/ru/xakaton/bimit/service/ModelService.java
@@ -154,7 +154,8 @@ public class ModelService {
 					if (device.getMaxValue()!=null && device.getMinValue()!=null) {
 						
 						int val = (int) (150 + (avg-device.getMinValue()) * 100/(device.getMaxValue() - device.getMinValue()));
-						
+						if (val>255) val= 255;
+						if (val<150) val = 150;
 						//00FF33	00FF00	00CC00	33CC33	00CC33
 						//155-250
 						color="0,"+val+",51";
