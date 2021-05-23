46f54d54b0f02aa85fdf40b382ac364b676a96af
Alex <nomatter@mifors.com>
2021-05-23 04:58:12 +0400

    1

--- src/main/java/ru/xakaton/bimit/device/controller/DeviceController.java
+++ src/main/java/ru/xakaton/bimit/device/controller/DeviceController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import ru.xakaton.bimit.device.model.Alarm;
 import ru.xakaton.bimit.device.model.AlarmTimeLine;
 import ru.xakaton.bimit.device.model.Device;
 import ru.xakaton.bimit.device.service.DeviceService;
@@ -29,6 +28,16 @@ public class DeviceController {
 	@GetMapping("/devices")
 	public List<Device> devices(){
 		return deviceService.list();
+	}
+	
+	@GetMapping("/devices/types")
+	public DeviceType[] deviceTypes(){
+		return deviceService.deviceTypes();
+	}
+	
+	@GetMapping("/devices/states")
+	public DeviceState[] deviceStates(){
+		return deviceService.deviceStates();
 	}
 	
 	@PostMapping("/devices")
